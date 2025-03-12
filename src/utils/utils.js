056c73c25057a8270d3de2b82a1d9802ea847559
lochy2000 <lochlann_oht@hotmail.com>
2025-03-12 22:55:58 +0000

    pagination helper function for API data fetching. infinite scroll functionality for event listings

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,5 @@
-import { jwtDecode } from "jwt-decode"; 
+import { jwtDecode } from "jwt-decode";
+import axiosInstance from '../api/axiosDefaults';
 
 export const setTokenTimeStamp = (data) => {
     const refreshTokenTimeStamp = jwtDecode(data?.refresh_token).exp;
@@ -11,4 +12,30 @@ export const shouldRefreshToken = () => {
 
 export const removeTokenTimeStamp = () => {
     localStorage.removeItem("refreshTokenTimeStamp");
+};
+
+/**
+ * Fetches more data for infinite scrolling components
+ * 
+ * @param {Object} resource - The resource containing results and next URL
+ * @param {Function} setResource - Function to update the resource state
+ */
+export const fetchMoreData = async (resource, setResource) => {
+  try {
+    // Extract the URL for the next page from the resource
+    const { next } = resource;
+    if (next) {
+      // If there is a next page, fetch it
+      const { data } = await axiosInstance.get(next);
+      // Update the resource with the new data
+      setResource(prevResource => ({
+        ...prevResource,
+        next: data.next,
+        // Concatenate the new results with the existing ones
+        results: [...prevResource.results, ...data.results],
+      }));
+    }
+  } catch (err) {
+    console.error(err);
+  }
 };
