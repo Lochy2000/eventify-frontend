1f95666478bcf6ece1ee9412cbb09d29142c88aa
lochy2000 <lochlann_oht@hotmail.com>
2025-03-14 11:17:47 +0000

    barrel exports for events components and pages

---
+++ src/components/events/index.js
@@ -0,0 +1,6 @@
+// Export all event-related components
+export { default as EventAttendButton } from './EventAttendButton';
+export { default as EventCard } from './EventCard';
+export { default as EventDetail } from './EventDetail';
+export { default as EventForm } from './EventForm';
+export { default as EventList } from './EventList';
---
+++ src/pages/events/index.js
@@ -0,0 +1,6 @@
+// Export all event-related pages
+export { default as EventsPage } from './EventsPage';
+export { default as EventDetailPage } from './EventDetailPage';
+export { default as EventCreatePage } from './EventCreatePage';
+export { default as EventEditPage } from './EventEditPage';
+export { default as EventAttendeesPage } from './EventAttendeesPage';
