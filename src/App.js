df04222b326167b1b041ccca80eda2d4e03e547d
lochy2000 <lochlann_oht@hotmail.com>
2025-03-16 17:16:57 +0000

    updated to use route paths for the serach filters on events

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Component imports
 import NavBar from './components/common/NavBar';
+import TestImageUpload from './components/debug/TestImageUpload';
 
 import SignInForm from './pages/auth/SignInForm';
 import SignUpForm from './pages/auth/SignUpForm';
 import ProfilePage from './pages/ProfilePage';
 import PeoplePage from './pages/PeoplePage';
+import ProfileEditForm from './pages/ProfileEditForm';
 import { EventsPage, EventDetailPage, EventCreatePage, EventEditPage, EventAttendeesPage } from './pages/events';
 
 import Footer from './components/common/Footer';
@@ -44,7 +46,9 @@ function App() {
                 <Route path="/attending" element={<EventsPage />} />
                 <Route path="/favorites" element={<EventsPage />} />
                 <Route path="/profile/:username" element={<ProfilePage />} />
+                <Route path="/profile/:username/edit" element={<ProfileEditForm />} />
                 <Route path="/people" element={<PeoplePage />} />
+                <Route path="/test-upload" element={<TestImageUpload />} />
                 <Route path="*" element={<NotFoundPage />} />
               </Routes>
             </main>
