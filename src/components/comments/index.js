1295d15723307dc95cd6c921885703c38abca695
lochy2000 <lochlann_oht@hotmail.com>
2025-03-17 13:14:55 +0000

    index.js to export all comment components

---
+++ src/components/comments/index.js
@@ -0,0 +1,4 @@
+// Export all comment-related components
+export { default as Comment } from './Comment';
+export { default as CommentList } from './CommentList';
+export { default as CommentForm } from './CommentForm';
