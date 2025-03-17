8331710654059e9bcc86392426186244438edb16
lochy2000 <lochlann_oht@hotmail.com>
2025-03-17 11:14:50 +0000

    export all common components

---
+++ src/components/common/index.js
@@ -0,0 +1,5 @@
+// Export all common components
+export { default as Asset } from './Asset';
+export { default as Avatar } from './Avatar';
+export { default as Footer } from './Footer';
+export { default as NavBar } from './NavBar';
